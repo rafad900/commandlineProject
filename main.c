737cee21eceff27157d3013495e50910839cdbfd
Mark Gondree <gondree@gmail.com>
2020-04-13 12:13:19 -0700

    Project starter code and tests for lobo-shell project in CS450

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include "constants.h"
+#include "parsetools.h"
+
+
+int main()
+{
+    // Buffer for reading one line of input
+    char line[MAX_LINE_CHARS];
+    // holds separated words based on whitespace
+    char* line_words[MAX_LINE_WORDS + 1];
+
+    // Loop until user hits Ctrl-D (end of input)
+    // or some other input error occurs
+    while( fgets(line, MAX_LINE_CHARS, stdin) ) {
+        int num_words = split_cmd_line(line, line_words);
+
+        for (int i=0; i < num_words; i++) {
+            printf("%s\n", line_words[i]);
+        }
+    }
+
+    return 0;
+}
+
+
